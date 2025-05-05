117ecee3f1b68d3b0590354db0c58ccb3fd920e2
idaob <202992564+idaob@users.noreply.github.com>
2025-05-05 20:19:20 +0200

    tomma rader i theme.h för tydligare uppdelning

--- include/theme.h
+++ include/theme.h
@@ -10,6 +10,7 @@ typedef struct audio Audio;
 Background *createBackground(SDL_Renderer *pRenderer, SDL_Rect *pScreenRect, State theme_type);
 void drawBackground(Background *pBackground, int CamX, int CamY);
 void destroyBackground(Background *pBackground);
+
 Button *createButton(SDL_Renderer *pRenderer, SDL_Rect *pScreenRect, ButtonType button_type);
 int setButtonPlacement(Button *pButton, ButtonType button_type);
 void drawButton(Button *pButton);
@@ -19,6 +20,7 @@ void toggleHoveredButton(Button *pButton);
 bool isButtonHovered(Button *pButton);
 bool isMouseOverButton(int x, int y, Button *pButton);
 void destroyButton(Button *pButton);
+
 Audio *createAudio(State theme_type);
 void playMusic(Audio *pAudio);
 void playButtonSound(Audio *pAudio);
@@ -27,6 +29,7 @@ void playDeathSound(Audio *pAudio);
 void toggleMuteAudio(Audio *pAudio);
 bool isMusicMuted(Audio *pAudio);
 void destroyAudio(Audio *pAudio);
+
 SDL_Cursor *initCursor();
 void destroyCursor(SDL_Cursor *pCursor);
 
