3369f14012b7f014a51a03984152f317f3514afd
OrkhonBoldbaatar <orkhonboldbaatar@gmail.com>
2025-05-06 14:30:11 +0200

    bytte cameran tillbaka

--- src/player.c
+++ src/player.c
@@ -136,8 +136,8 @@ SDL_Surface *initPlayerFrames(Player *pPlayer, int player_ID)
 void initStartPosition(Player *pPlayer, SDL_Rect blockRect)
 {
     pPlayer->oldX = pPlayer->x = blockRect.w * 5;
-    pPlayer->oldY = pPlayer->y = BOX_SCREEN_Y * blockRect.h - blockRect.h * 2 - pPlayer->dstRect.h;
-    // pPlayer->oldY = pPlayer->y = 0;
+    // pPlayer->oldY = pPlayer->y = BOX_SCREEN_Y * blockRect.h - blockRect.h * 2 - pPlayer->dstRect.h;
+    pPlayer->oldY = pPlayer->y = 0;
     /*
     pPlayer->dstRect.x = (float)(pPlayer->pScreenRect->x + blockRect.w / 2);  // Börjar i mitten av ett block i x-led
     pPlayer->dstRect.y = (float)(pPlayer->pScreenRect->y + pPlayer->pScreenRect->h - blockRect.h - pPlayer->frames.characterRect.h);
