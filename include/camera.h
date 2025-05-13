7dbc5b5394cf4ffe3f9ee40b0af5a37eeb8a6f76
DennisBica <dennis.bica@gmail.com>
2025-05-13 22:33:20 +0200

    vinst

--- include/camera.h
+++ include/camera.h
@@ -8,7 +8,7 @@ typedef struct view View;
 Camera *createCamera(SDL_Rect *pScreenRect);
 int getCamX(Camera *pCamera);
 int getCamY(Camera *pCamera);
-void updateCamera(Camera *pCamera, int targetX, int targetY);
+void updateCamera(Camera *pCamera, int targetX, float targetY);
 void destroyCamera(Camera *pCamera);
 
 #endif
